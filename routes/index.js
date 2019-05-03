5fa5959121d3a8795d5079f9f8247bac99fb5d37
Annelies Bellon <annelies_bellon@hotmail.com>
2019-05-03 09:54:08 +0200

    add basic routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+let messageController = require('../controllers/message');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// show all chats and get current chats
+router.get('/', messageController.get);
+
+// router GET: /api/v1/messages OR /api/v1/messages?user=username
+router.get('/api/v1/messages', messageController.get);
+
+// router GET: /api/v1/messages/:id
+router.get('/api/v1/messages/:id', messageController.getid);
+
+// router POST: /api/v1/messages
+router.post('/api/v1/messages', messageController.post);
+
+// router PUT: /api/v1/messages/:id
+router.put('/api/v1/messages/:id', messageController.putid);
+
+// router DELETE: /api/v1/messages/:id
+router.delete('/api/v1/messages/:id', messageController.del);
+
 
 module.exports = router;
