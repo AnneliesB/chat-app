e2827338a4dea5508133dc8570a06c6378b07693
Annelies Bellon <annelies_bellon@hotmail.com>
2019-05-16 21:43:04 +0200

    move js to src and dist folders

---
+++ public/javascripts/src/loadMessage.js
@@ -0,0 +1,65 @@
+fetch('http://localhost:3000/api/v1/messages', {
+        method: "get",
+        "headers": {
+            "Content-Type": 'application/json',
+            "Authorization": "Bearer " + localStorage.getItem('token')
+        }
+    })
+    .then(result => {
+        return result.json();
+    })
+    .then(json => {
+        console.log(json);
+        console.log(json.data.messages.length);
+        console.log(json.data.messages[0].user);
+        json.data.messages.forEach(element => {
+            console.log(element);
+            let message = `<ul class="message__container message--sent">
+                        <li class="message__avatar"></li>
+                        <li class="message__user">${element.user}</li>
+                        <li>
+                        <p>${element.message}</p>
+                        </li>
+                        </ul>`;
+            document.querySelector(".messages__flex").insertAdjacentHTML('beforeend', message);
+        });
+        
+            
+
+        
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+/*const load = (req, res, next) => {
+    fetch('http://localhost:3000/api/v1/messages', {
+        method: "get",
+        "headers": {
+            "Content-Type": 'application/json',
+            "Authorization": "Bearer " + localStorage.getItem('token')
+        }
+    }).then(result => {
+        return result.json();
+    }).then(json => {
+        //console.log(json);
+        for (var i = 0; i < json.length; i++) {
+            let message = `<ul class="message__container message--sent">
+                        <li class="message__avatar"></li>
+                        <li class="message__user">${json.data.message.user}</li>
+                        <li>
+                        <p>${json.data.message.message}</p>
+                        </li>
+                        </ul>`;
+            document.querySelector(".loadedMessages").insertAdjacentHTML('beforeend', message);
+
+        }
+
+    }).catch(err =>{
+        console.log
+    })
+
+
+}
+
+load();*/
