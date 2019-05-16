13bd2d112694bf8d9722101457d44d77a8439f97
Annelies Bellon <annelies_bellon@hotmail.com>
2019-05-16 22:43:34 +0200

    add nodemon gulp script to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ const {src, dest, watch, parallel} = require('gulp');
 const html2pug = require('gulp-html2pug');
 const imagemin = require("gulp-imagemin");
 const sass = require('gulp-sass');
-const babel = require("gulp-babel")
+const babel = require("gulp-babel");
+const nodemon = require('gulp-nodemon');
 
 function htmlTopug(done){
     src("./public/html/*.html")
@@ -31,10 +32,18 @@ function es6ToVanilla(done){
     done();
 }
 
+function gulpNodemon(done){
+    nodemon({
+        script: './bin/www'
+    })
+    done();
+}
+
 watch("./public/html/**/*.html", htmlTopug);
 watch("./public/images/*.*", compressImages);
 watch("./public/stylesheets/source/**/*.scss", sass2css);
 watch('./public/javascripts/src/*.js', es6ToVanilla);
 
-module.exports.default = parallel(sass2css, compressImages, es6ToVanilla);
+module.exports.default = parallel(sass2css, compressImages, es6ToVanilla, gulpNodemon);
 module.exports.htmltopug = htmlTopug;
+//module.exports.nodemon = gulpNodemon;
