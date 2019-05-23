9b1460ba003084d8177fcf0c2e909282c6bcb960
MorroCodes <46780762+MorroCodes@users.noreply.github.com>
2019-05-24 00:24:05 +0200

    deploy

--- public/javascripts/src/config.js
+++ public/javascripts/src/config.js
@@ -1,2 +1,2 @@
-//var url = "https://secret-savannah-51030.herokuapp.com/";
-var url = "http://localhost:3000/";
+var url = "https://secret-savannah-51030.herokuapp.com/";
+// var url = "http://localhost:3000/";
